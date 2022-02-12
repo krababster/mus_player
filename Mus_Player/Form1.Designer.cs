77339e573c8b475123abfbf1a8427977b70ce4ea
student <student@COMP-507-1-03>
2022-02-12 12:52:02 +0200

    Added Listbox

--- Mus_Player/Form1.Designer.cs
+++ Mus_Player/Form1.Designer.cs
@@ -1,4 +1,7 @@
-﻿namespace Mus_Player
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Mus_Player
 {
     partial class Form1
     {
@@ -30,10 +33,44 @@
         {
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 800);
             this.Text = "Form1";
-        }
 
+            this.CD.Image = Image.FromFile("disk-dvdr-4-7gb-16h-vulk-(50)-d-54099-085-mix.jpg");
+            this.CD.Location = new Point(250, 0);
+            this.CD.Size = new Size(300, 290);
+            this.Controls.Add(CD);
+
+            this.PrevTrack.Location = new Point(250, 300);
+            this.PrevTrack.Size = new Size(50, 50);
+            this.PrevTrack.Image = Image.FromFile("Prev.png");
+            this.Controls.Add(PrevTrack);
+
+            this.PlayStop.Location = new Point(this.PrevTrack.Location.X + 125, this.PrevTrack.Location.Y);
+            this.PlayStop.Size = new Size(50, 50);
+            this.PlayStop.Image = Image.FromFile("Play.png");
+            this.Controls.Add(PlayStop);
+
+            this.NextTrack.Location = new Point(this.PlayStop.Location.X + 125, this.PlayStop.Location.Y);
+            this.NextTrack.Size = new Size(50, 50);
+            this.NextTrack.Image = Image.FromFile("Next.png");
+            this.Controls.Add(NextTrack);
+
+            this.listbox.Location = new Point(100, 400);
+            this.listbox.Size = new Size(600, 300);
+            this.Controls.Add(listbox);
+
+
+        }
+        PictureBox CD = new PictureBox();
+
+        Button PrevTrack = new Button();
+        Button NextTrack = new Button();
+        Button PlayStop = new Button();
+
+        OpenFileDialog add = new OpenFileDialog();
+
+        ListBox listbox = new ListBox();
         #endregion
     }
 }
